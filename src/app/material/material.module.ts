a0633aa4ddb632ac8f8d1d942eaa4af518a63982
Avinash Singh <Avinash.Singh@brandmuscle.com>
2020-10-29 17:30:07 +0530

    materialdemo

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import {MatButtonModule} from '@angular/material';
+@NgModule({
+  declarations: [],
+  imports: [
+    
+  ]
+})
+export class MaterialModule { }
